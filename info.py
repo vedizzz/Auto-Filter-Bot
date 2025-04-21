ae956db31029e33051f5d4919c5105ff9255e039
vedizzz <nithishtk6@gmail.com>
2025-04-21 18:21:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ SPELL_CHECK = is_enabled("SPELL_CHECK", True)
 SHORTLINK = is_enabled('SHORTLINK', True)
 
 # for stream
-IS_STREAM = is_enabled('IS_STREAM', True)
+IS_STREAM = is_enabled('IS_STREAM', False)
 BIN_CHANNEL = environ.get("BIN_CHANNEL", "-1002250340602")
 if len(BIN_CHANNEL) == 0:
     logger.error('BIN_CHANNEL is missing, exiting now')
